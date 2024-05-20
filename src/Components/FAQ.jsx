b6d6eed41ce53e8cdff2025d65065f7ca1566d07
NicolasMurillo <nicolas.murillo@fenixalliance.com>
2024-05-20 00:53:29 -0500

    [fix] little fix

--- src/Components/FAQ.jsx
+++ src/Components/FAQ.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import Single from '../assets/single.png'
-import Double from '../assets/double.png'
-import Triple from '../assets/triple.png'
+
 
 const FAQ = () => {
   return (
